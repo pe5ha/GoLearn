3439a239d266fbe63fa9bacf4d607d04bb6299cf
pe5ha <pavel.naumenko.2000@yandex.ru>
2021-03-27 17:18:46 +0300

    update 5 +игровой движок работает +кнопки Пас и Вернуть работают

--- app/src/main/java/ru/johnlayming/goboard/GameState.java
+++ app/src/main/java/ru/johnlayming/goboard/GameState.java
@@ -12,9 +12,12 @@ public class GameState {
     public int whoseTurn;
     public int opponent;
     public int typeofLastMove; // 0 - move, 1 - pass, 2 - return, -1 - suicide, -2 - rule Ko, -3 - resign
-    public int[] ko;
+    //public int[] ko;
     public int koTurn;
-    public boolean koRule;
+    public boolean koCanBeNow;
+    public boolean lastKoCanBeNow;
+    public boolean koRuleNow;
+    public Coordinates koCanBeHere;
     public int win; // 0, 1 - black win, 2 - white win
     public int moveX;
     public int moveY;
@@ -28,29 +31,32 @@ public class GameState {
 
 
     public GameState(GameUI gameUI,int size){
+
+        // INITIALIZATION
         this.gameUI=gameUI;
         this.size=size;
-        this.board = new byte[size][size];
-        this.turn=1;
-        this.whoseTurn=1;
-        this.opponent=2;
-        this.win=0;
-        ko = new int[2];
-        koTurn=-2;
-        capturedStones=0;
+        // main of game state
+        board = new byte[size][size]; // собственно игровая доска в которой 0 - пусто, 1 - черный камень, 2 - белый
+        turn=1; // ход первый
+        whoseTurn=1; // первый ход черных
+        opponent=2; // оппонент на первом ходу белый
+        win=0; // никто пока не победил
+        // ko flags
+        //ko = new int[2];
+        koRuleNow = false;
+        koCanBeNow = false;
+        koCanBeHere = new Coordinates(-1,-1);
+        koTurn=-1;
+        capturedStones=0; // чтобы понимать группа из скольких камней была захвачена. если 1 камень - дальше возможно ко
+        // Array group for search his dame
         group = new ArrayList<>();
+        // Array stonesHistory for save spawn turn and status each stone
         stonesHistory = new ArrayList<>();
     }
-
-    public void setState(byte[][] p){
-        this.board=p;
-    }
-
 
     public boolean attemptMove(int x,int y){ // -1 - пас -2 - сдача
         moveX=x;
         moveY=y;
-        koRule=false;
         if(board[y][x]==0){ //если пункт не занят
 
             group.clear();
@@ -68,33 +74,51 @@ public class GameState {
                 lastMoveX=moveX;
                 lastMoveY=moveY;
                 typeofLastMove=0;
-
-                return true;
+                koRuleNow=false;
+                if(capturedStones!=1){
+                    lastKoCanBeNow=koCanBeNow;
+                    koCanBeNow=false;
+                }
+                return true; // ХОД СДЕЛАН
             }
-            else if(koRule){
-                board[lastMoveY][lastMoveX]= (byte) opponent;
-                gameUI.addStone(lastMoveX,lastMoveY,opponent);
-                board[y][x] = 0;
+            // ПРАВИЛО КО ОПРЕДЕЛЯЕТСЯ НА МОМЕНТЕ ЗАХВАТИТЬ КАМЕНЬ (В tryCapture())
+            else if(koRuleNow&&koCanBeHere.x==moveX&&koCanBeHere.y==moveY){
+                board[moveY][moveX] = 0;
                 typeofLastMove=-2;
                 gameUI.koMove();
                 //KO!
             }
+
             else{
                 if(searchDame(x,y,whoseTurn)){
-                    makeMove(x,y);
-                    return true;
+                    //makeMove(x, y);
+
+                    // У КАМНЯ ЕСТЬ ДАМЭ, СДЕЛАТЬ ХОД
+                    stonesHistory.add(new Stone(moveX,moveY,whoseTurn,turn));
+                    board[y][x] = (byte) (whoseTurn); // чей сейчас ход делается белых или черных на основе количества ходов
+                    gameUI.addStone(x,y,whoseTurn);
+                    turn++;
+                    opponent=whoseTurn;
+                    whoseTurn = whoseTurn == 1 ? 2 : 1;
+                    lastMoveX=moveX;
+                    lastMoveY=moveY;
+                    typeofLastMove=0;
+                    koRuleNow=false;
+                    lastKoCanBeNow=koCanBeNow;
+                    koCanBeNow=false;
+                    return true; // ХОД СДЕЛАН
                 }
                 else {
+
+                    // У КАМНЯ НЕТ ДАМЭ, SUICIDE MOVE!!!
                     board[y][x] = 0;
                     typeofLastMove=-1;
                     gameUI.suicideMove();
-                    //SUICIDE MOVE!!!
                 }
             }
         }
 
-        group.clear();
-        return false;
+        return false; // ХОД НЕ СДЕЛАН. КО ЛИБО САМОУБИЙСТ
     }
 
     public boolean Capture(int x,int y){
@@ -121,50 +145,60 @@ public class GameState {
             }
         }
 
-        if(cpt>0) {
-            if(koTurn==turn-1){
-                if(ko[0]==moveX && ko[1]==moveY){
-                    //ko!
-                    koRule=true;
-                    return false;
-                }
-            }
+        if(cpt>1) {
+//            if(koTurn==turn-1){
+//                if(ko[0]==moveX && ko[1]==moveY){
+//                    //ko!
+//                    koRule=true;
+//                    return false;
+//                }
+//            }
+            return true;  // ГРУППА ЗАХВАЧЕНА
+        }
+        if(cpt==1&&!(koCanBeNow&&koCanBeHere.x==moveX&&koCanBeHere.y==moveY)){
             return true;
         }
-        else return false;
+        return false;
     }
 
     public boolean tryCapture(int x,int y){
         group.clear();
         if(!searchDame(x,y,opponent)){
             capturedStones+=group.size();
-            if(capturedStones==1){
-                if(koTurn==turn-1){
 
+            // ОПРЕДЕЛЕНИЕ КО
+            if(capturedStones==1){
+                if(koCanBeNow){
+                    if(koCanBeHere.x==moveX&&koCanBeHere.y==moveY){
+                        koRuleNow=true;
+
+                        return false; // ГРУППА НЕ ЗАХВАЧЕНА: ПРАВИЛО КО
+                    }
+                    else{
+                        // флаг koCanBeNow сбрасывается при каждом успешном ходе(не ко и не суицд)
+                    }
                 }
-                else {
-                    ko[0] = x;
-                    ko[1] = y;
-                    koTurn=turn;
-                }
+
+                // заряжжаем триггер
+                koCanBeNow=true;
+                koCanBeHere.x=x;
+                koCanBeHere.y=y;
             }
-            else {
-                koTurn--;
-            }
+
+
             for (int i = 0; i < group.size(); i++) {
-                if(!koRule) stonesHistory.get(findStoneByXY(group.get(i).x,group.get(i).y)).turnOfDie=turn;
+                stonesHistory.get(findStoneByXY(group.get(i).x,group.get(i).y)).turnOfDie=turn;
                 board[group.get(i).y][group.get(i).x]=0;
                 gameUI.removeStone(group.get(i).x,group.get(i).y);
             }
-            return true;
+            return true; // ГРУППА ЗАХВАЧЕНА: У ГРУППЫ ПРОТИВНИКА НЕТ ДАМЭ, ПРАВИЛО КО НЕ ВОЗНИКЛО
         }
-        //group.clear();
-        return false;
-
+        group.clear();
+        return false; // ГРУППА НЕ ЗАХВАЧЕНА: У ГРУППЫ ПРОТИВНИКА ЕСТЬ ДАМЭ
     }
 
-    public boolean searchDame(int x,int y,int who){
-        for(int i=0;i<group.size();i++){
+    public boolean searchDame(int x,int y,int who){ //СУПЕР ПУПЕР РЕКУРСИВНЫЙ АЛГОРИТМ
+        for(int i=0;i<group.size();i++){ // проверка, а не проверяли ли мы уже этот самый камень ранее
             if(group.get(i).x==x&&group.get(i).y==y) return false;
         }
         group.add(new Coordinates(x,y));
@@ -218,25 +252,11 @@ public class GameState {
 
             stonesHistory.remove(stonesHistory.size()-1);
 
+            koCanBeNow=lastKoCanBeNow;
             turn--;
             whoseTurn = whoseTurn == 1 ? 2 : 1;
             opponent = opponent == 1 ? 2 : 1;
             typeofLastMove = 2; // return
-        }
-    }
-
-    public boolean koDetector(){
-        boolean koCanBeNow;
-        Coordinates koCanBeHere;
-        if(koCanBeNow&&koCanBeHere.x==moveX&&koCanBeHere.y=moveY){
-
-
-            return true;
-        }
-        else {
-            if (capturedStones==1&&)
-
-            return false;
         }
     }
 
@@ -245,6 +265,8 @@ public class GameState {
             endGame();
             gameUI.gameEnd();
         } else {
+            koCanBeNow=false;
+            koCanBeHere.x=-1;koCanBeHere.y=-1;
             opponent=whoseTurn;
             whoseTurn = whoseTurn == 1 ? 2 : 1;
             typeofLastMove=1;
@@ -270,6 +292,11 @@ public class GameState {
 
     public void endGame(){
 
+    }
+
+    public void setState(byte[][] p){
+
+        this.board=p;
     }
 
     public void placeStone(int x,int y,int stone){
